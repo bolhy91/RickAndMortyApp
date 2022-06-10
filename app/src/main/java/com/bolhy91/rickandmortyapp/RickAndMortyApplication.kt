1147300230f894abcdb9426db3dfae0480ba21eb
bolhy91 <bolhy91@gmail.com>
2022-06-10 00:30:22 -0500

    Dependencias y configuracion

---
+++ app/src/main/java/com/bolhy91/rickandmortyapp/RickAndMortyApplication.kt
@@ -0,0 +1,7 @@
+package com.bolhy91.rickandmortyapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class RickAndMortyApplication : Application()
---
+++ app/src/main/java/com/bolhy91/rickandmortyapp/presentation/CharacterList.kt
@@ -0,0 +1,2 @@
+package com.bolhy91.rickandmortyapp.presentation
+
